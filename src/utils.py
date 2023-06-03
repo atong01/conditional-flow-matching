2b90d979b7be0b71644b6a665757a3650d2ab8b8
Kilian <kilian.fatras.pro@gmail.com>
2023-06-02 22:53:07 -0400

    add utils file

---
+++ src/utils.py
@@ -0,0 +1,69 @@
+import math
+import numpy as np
+import torch
+import torchdyn
+from torchdyn.datasets import generate_moons
+
+savedir = "models/8gaussian-moons"
+os.makedirs(savedir, exist_ok=True)
+
+# Implement some helper functions
+
+
+def eight_normal_sample(n, dim, scale=1, var=1):
+    m = torch.distributions.multivariate_normal.MultivariateNormal(
+        torch.zeros(dim), math.sqrt(var) * torch.eye(dim)
+    )
+    centers = [
+        (1, 0),
+        (-1, 0),
+        (0, 1),
+        (0, -1),
+        (1.0 / np.sqrt(2), 1.0 / np.sqrt(2)),
+        (1.0 / np.sqrt(2), -1.0 / np.sqrt(2)),
+        (-1.0 / np.sqrt(2), 1.0 / np.sqrt(2)),
+        (-1.0 / np.sqrt(2), -1.0 / np.sqrt(2)),
+    ]
+    centers = torch.tensor(centers) * scale
+    noise = m.sample((n,))
+    multi = torch.multinomial(torch.ones(8), n, replacement=True)
+    data = []
+    for i in range(n):
+        data.append(centers[multi[i]] + noise[i])
+    data = torch.stack(data)
+    return data
+
+
+def sample_moons(n):
+    x0, _ = generate_moons(n, noise=0.2)
+    return x0 * 3 - 1
+
+
+def sample_8gaussians(n):
+    return eight_normal_sample(n, 2, scale=5, var=0.1).float()
+
+
+class torch_wrapper(torch.nn.Module):
+    """Wraps model to torchdyn compatible format."""
+
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
+
+    def forward(self, t, x):
+        return model(torch.cat([x, t.repeat(x.shape[0])[:, None]], 1))
+
+
+def plot_trajectories(traj):
+    """
+    Plot trajectories of some selected samples
+    """
+    n = 2000
+    plt.figure(figsize=(6, 6))
+    plt.scatter(traj[0, :n, 0], traj[0, :n, 1], s=10, alpha=0.8, c="black")
+    plt.scatter(traj[:, :n, 0], traj[:, :n, 1], s=0.2, alpha=0.2, c="olive")
+    plt.scatter(traj[-1, :n, 0], traj[-1, :n, 1], s=4, alpha=1, c="blue")
+    plt.legend(["Prior sample z(S)", "Flow", "z(0)"])
+    plt.xticks([])
+    plt.yticks([])
+    plt.show()
