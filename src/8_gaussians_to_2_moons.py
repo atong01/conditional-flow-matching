d152743f661e714dc2eeb032267b8eeb10c02183
Kilian Fatras <kilian.fatras.pro@gmail.com>
2023-06-02 23:29:31 -0400

    add ot_cfm toy

--- src/8_gaussians_to_2_moons.py
+++ src/8_gaussians_to_2_moons.py
@@ -61,9 +61,47 @@ for k in range(20000):
                 t_span=torch.linspace(0, 1, 100),
             )
             plot_trajectories(traj)
-            plt.savefig('results/generated_samples_{}_iter'.format(k))
+            plt.savefig('results/cfm_generated_samples_{}_iter'.format(k))
 torch.save(model, f"{savedir}/cfm_v1.pt")
 
 #################################
 #            OT-CFM
 #################################
+
+sigma = 0.1
+dim = 2
+batch_size = 256
+model = MLP(dim=dim, time_varying=True)
+optimizer = torch.optim.Adam(model.parameters())
+FM = ExactOptimalTransportConditionalFlowMatching(sigma=0.1)
+
+start = time.time()
+for k in range(20000):
+    optimizer.zero_grad()
+
+    x0 = sample_8gaussians(batch_size)
+    x1 = sample_moons(batch_size)
+
+    t, xt, ut = FM.sample_location_and_conditional_flow(x0, x1)
+
+    vt = model(torch.cat([xt, t], dim=-1))
+    loss = torch.mean((vt - ut) ** 2)
+    
+    loss.backward()
+    optimizer.step()
+
+    if (k + 1) % 5000 == 0:
+        end = time.time()
+        print(f"{k+1}: loss {loss.item():0.3f} time {(end - start):0.2f}")
+        start = end
+        node = NeuralODE(
+            torch_wrapper(model), solver="dopri5", sensitivity="adjoint", atol=1e-4, rtol=1e-4
+        )
+        with torch.no_grad():
+            traj = node.trajectory(
+                sample_8gaussians(1024),
+                t_span=torch.linspace(0, 1, 100),
+            )
+            plot_trajectories(traj)
+            plt.savefig('results/ot_cfm_generated_samples_{}_iter'.format(k))
+torch.save(model, f"{savedir}/cfm_v1.pt")
