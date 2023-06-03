41c48e2d27b95c3a201f242c020e1cafa5a61542
Kilian <kilian.fatras.pro@gmail.com>
2023-06-02 22:53:24 -0400

    add toy file for sanity check

---
+++ src/8_gaussians_to_2_moons.py
@@ -0,0 +1,60 @@
+#################################
+# DIRTY FILE FOR SANITY CHECK
+#################################
+
+import math
+import os
+import time
+
+import matplotlib.pyplot as plt
+import numpy as np
+import ot as pot
+import torch
+import torchdyn
+from torchdyn.core import NeuralODE
+from torchdyn.datasets import generate_moons
+
+from utils import *
+from models import *
+from conditional_flow_macthing import *
+
+savedir = "models/8gaussian-moons"
+os.makedirs(savedir, exist_ok=True)
+
+%%time
+sigma = 0.1
+dim = 2
+batch_size = 256
+model = MLP(dim=dim, time_varying=True)
+optimizer = torch.optim.Adam(model.parameters())
+FM = ConditionalFlowMatching(sigma=0.1)
+
+start = time.time()
+for k in range(20000):
+    optimizer.zero_grad()
+
+    x0 = sample_8gaussians(batch_size)
+    x1 = sample_moons(batch_size)
+
+    t, xt, ut = FM.sample_location_and_conditional_flow(x0, x1)
+
+    vt = model(torch.cat([x, t], dim=-1))
+    loss = torch.mean((vt - ut) ** 2)
+    
+    loss.backward()
+    optimizer.step()
+
+    if (k + 1) % 5000 == 0:
+        end = time.time()
+        print(f"{k+1}: loss {loss.item():0.3f} time {(end - start):0.2f}")
+        start = end
+        node = NeuralODE(
+            torch_wrapper(model), solver="dopri5", sensitivity="adjoint", atol=1e-4, rtol=1e-4
+        )
+        with torch.no_grad():
+            traj = node.trajectory(
+                sample_8gaussians(1024),
+                t_span=torch.linspace(0, 1, 100),
+            )
+            plot_trajectories(traj)
+torch.save(model, f"{savedir}/cfm_v1.pt")
