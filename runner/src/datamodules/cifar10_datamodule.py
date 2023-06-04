d9b47214aa6f6fd239dc1558f17465166e91f74c
Alex Tong <alexandertongdev@gmail.com>
2023-06-04 08:00:27 -0400

    Add cifar

---
+++ runner/src/datamodules/cifar10_datamodule.py
@@ -0,0 +1,16 @@
+from typing import Any, List, Union
+
+import pl_bolts
+import torch
+from torch.utils.data import DataLoader
+from torchvision import transforms as transform_lib
+
+
+class CIFAR10DataModule(pl_bolts.datamodules.cifar10_datamodule.CIFAR10DataModule):
+    def __init__(self, *args, **kwargs):
+        test_transforms = transform_lib.ToTensor()
+        super().__init__(*args, test_transforms=test_transforms, **kwargs)
+
+    def val_dataloader(self, *args: Any, **kwargs: Any) -> Union[DataLoader, List[DataLoader]]:
+        """The val dataloader."""
+        return self._data_loader(self.dataset_train)
