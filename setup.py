01c9f9e4d2a7e637967daa08684d970f6c45cff5
Alex Tong <alexandertongdev@gmail.com>
2023-07-24 06:18:41 -0400

    make installable

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import find_packages, setup
 
 setup(
     name="torchcfm",
-    version="0.0.1",
-    description="Conditional Flow Matching in PyTorch",
-    author="",
-    author_email="",
+    version="1.0.0",
+    description="Conditional Flow Matching for Fast Continuous Normalizing Flow Training.",
+    author="Alexander Tong",
+    author_email="alexandertongdev@gmail.com",
     url="https://github.com/atong01/conditional-flow-matching",  # REPLACE WITH YOUR OWN GITHUB PROJECT LINK
-    install_requires=["pytorch-lightning", "hydra-core"],
+    install_requires=["torch", "pot", "numpy", "torchdyn"],
     packages=find_packages(),
 )
