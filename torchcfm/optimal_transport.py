a738b6139d25fd1eae0e49212cb308737634f68f
Kilian <kilian.fatras.pro@gmail.com>
2023-08-18 23:19:20 -0400

    get_map sample_map sample_plan doc

--- torchcfm/optimal_transport.py
+++ torchcfm/optimal_transport.py
@@ -8,8 +8,9 @@ import torch
 
 
 class OTPlanSampler:
-    """OTPlanSampler implements sampling coordinates according to an OT plan (wrt squared Euclidean
-    cost) with different implementations of the plan calculation."""
+    """OTPlanSampler implements sampling coordinates according to an OT plan
+    (wrt squared Euclidean cost) with different implementations of the plan
+    calculation."""
 
     def __init__(
         self,
@@ -26,9 +27,11 @@ class OTPlanSampler:
         elif method == "sinkhorn":
             self.ot_fn = partial(pot.sinkhorn, reg=reg)
         elif method == "unbalanced":
-            self.ot_fn = partial(pot.unbalanced.sinkhorn_knopp_unbalanced, reg=reg, reg_m=reg_m)
+            self.ot_fn = partial(pot.unbalanced.sinkhorn_knopp_unbalanced,
+                                 reg=reg, reg_m=reg_m)
         elif method == "partial":
-            self.ot_fn = partial(pot.partial.entropic_partial_wasserstein, reg=reg)
+            self.ot_fn = partial(pot.partial.entropic_partial_wasserstein,
+                                 reg=reg)
         else:
             raise ValueError(f"Unknown method: {method}")
         self.reg = reg
@@ -37,6 +40,22 @@ class OTPlanSampler:
         self.kwargs = kwargs
 
     def get_map(self, x0, x1):
+        """
+        Compute the OT plan (wrt squared Euclidean cost) between a source and a
+        target minibatch.
+
+        Parameters
+        ----------
+        x0 : Tensor, shape (bs, *dim)
+            represents the source minibatch
+        x1 : Tensor, shape (bs, *dim)
+            represents the source minibatch
+
+        Returns
+        -------
+        p : numpy array, shape (bs, bs)
+            represents the OT plan between minibatches
+        """
         a, b = pot.unif(x0.shape[0]), pot.unif(x1.shape[0])
         if x0.dim() > 2:
             x0 = x0.reshape(x0.shape[0], -1)
@@ -55,12 +74,47 @@ class OTPlanSampler:
         return p
 
     def sample_map(self, pi, batch_size):
+        r"""
+        Draw source and target samples from pi  $(x,z) \sim \pi$
+
+        Parameters
+        ----------
+        pi : numpy array, shape (bs, bs)
+            represents the source minibatch
+        batch_size : int
+            represents the OT plan between minibatches
+
+        Returns
+        -------
+        (i_s, i_j) : tuple of numpy arrays, shape (bs, bs)
+            represents the indices of source and target data samples from $\pi$
+        """
         p = pi.flatten()
         p = p / p.sum()
-        choices = np.random.choice(pi.shape[0] * pi.shape[1], p=p, size=batch_size)
+        choices = np.random.choice(pi.shape[0] * pi.shape[1], p=p,
+                                   size=batch_size)
         return np.divmod(choices, pi.shape[1])
 
     def sample_plan(self, x0, x1):
+        r"""
+        Compute the OT plan $\pi$ (wrt squared Euclidean cost) between a source
+        and a target minibatch and draw source and target samples from pi
+        $(x,z) \sim \pi$
+
+        Parameters
+        ----------
+        x0 : Tensor, shape (bs, *dim)
+            represents the source minibatch
+        x1 : Tensor, shape (bs, *dim)
+            represents the source minibatch
+
+        Returns
+        -------
+        x0[i] : Tensor, shape (bs, *dim)
+            represents the source minibatch drawn from $\pi$
+        x1[j] : Tensor, shape (bs, *dim)
+            represents the source minibatch drawn from $\pi$
+        """
         pi = self.get_map(x0, x1)
         i, j = self.sample_map(pi, x0.shape[0])
         return x0[i], x1[j]
