a659457163f2c278b5b32e0c9426af4624158174
Kilian <kilian.fatras.pro@gmail.com>
2023-11-06 11:04:35 -0500

    add tests over guidance functions

--- test/test_time_t.py
+++ test/test_time_t.py
@@ -16,16 +16,35 @@ seed=1994
 batch_size=128
 
 def test_random_Tensor_t(FM):
+    # Test sample_location_and_conditional_flow functions
     x0 = torch.randn(batch_size, 2)
     x1 = torch.randn(batch_size, 2)
+
     torch.manual_seed(seed)
     t_given = torch.rand(batch_size)
-    t, xt, ut = FM.sample_location_and_conditional_flow(x0, x1, t=t_given)
+    t_given, xt, ut = FM.sample_location_and_conditional_flow(x0, x1, t=t_given)
     
     torch.manual_seed(seed)
     t_random, xt, ut = FM.sample_location_and_conditional_flow(x0, x1, t=None)
+    
+    assert any(t_given==t_random)
+
+def guided_test_random_Tensor_t(FM):
+    # Test guided_sample_location_and_conditional_flow functions
+    x0 = torch.randn(batch_size, 2)
+    y0 = torch.randint(high=10, size=(batch_size,1))
+    x1 = torch.randn(batch_size, 2)
+    y1 = torch.randint(high=10, size=(batch_size,1))
+    
+    torch.manual_seed(seed)
+    t_given = torch.rand(batch_size)
+    t_given, xt, ut, y0, y1 = FM.guided_sample_location_and_conditional_flow(x0, x1, y0=y0, y1=y1, t=t_given)
+    
+    torch.manual_seed(seed)
+    t_random, xt, ut, y0, y1 = FM.guided_sample_location_and_conditional_flow(x0, x1, y0=y0, y1=y1, t=None)
 
     assert any(t_given==t_random)
+    
 
 ICFM = ConditionalFlowMatcher(sigma=0.0)
 OTCFM = ExactOptimalTransportConditionalFlowMatcher(sigma=0.0)
@@ -37,3 +56,9 @@ list_FM = [ICFM, OTCFM, SBCFM, FM, SI]
 
 for FM in list_FM:
     test_random_Tensor_t(FM)
+
+
+list_FM = [OTCFM, SBCFM]
+
+for FM in list_FM:
+    guided_test_random_Tensor_t(FM)
