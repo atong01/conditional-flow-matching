7b534c8f77edd1552c2fdc9c50f35add527567ee
Kilian <kilian.fatras.pro@gmail.com>
2023-11-12 16:47:01 -0500

    add OT-CFM test

--- test/test_conditional_flow_matcher.py
+++ test/test_conditional_flow_matcher.py
@@ -4,6 +4,8 @@
 
 import pytest
 import torch
+import numpy as np
+from torchcfm.optimal_transport import OTPlanSampler
 
 from torchcfm.conditional_flow_matching import (
     ConditionalFlowMatcher,
@@ -20,18 +22,50 @@ sigma = 0.5
     "FM",
     [
         ConditionalFlowMatcher(sigma=sigma),
-        #ExactOptimalTransportConditionalFlowMatcher(sigma=0.1),
     ],
 )
 def test_random_sample_location_and_vector_field(FM):
     # Test sample_location_and_conditional_flow functions
-    x0 = torch.randn(batch_size, 2)
-    x1 = torch.randn(batch_size, 2)
+    x0 = torch.randn(batch_size, 2, 2)
+    x1 = torch.randn(batch_size, 2, 2)
 
     torch.manual_seed(seed)
     t, xt, ut, eps = FM.sample_location_and_conditional_flow(x0, x1, return_noise=True)
 
     torch.manual_seed(seed)
+    sigma_t = torch.FloatTensor([sigma]).reshape(-1, *([1] * (x0.dim() - 1))) 
+    t_given = torch.rand(batch_size)
+    t_given = t_given.reshape(-1, *([1] * (x0.dim() - 1)))
+
+    epsilon = torch.randn_like(x0)
+    mu_t = t_given * x1 + (1 - t_given) * x0
+    computed_xt = mu_t + sigma_t * epsilon
+    computed_ut = x1 - x0
+
+    assert torch.all(ut.eq(computed_ut))
+    assert torch.all(xt.eq(computed_xt))
+    assert torch.all(eps.eq(epsilon))
+
+
+@pytest.mark.parametrize(
+    "FM",
+    [
+        ExactOptimalTransportConditionalFlowMatcher(sigma=sigma),
+    ],
+)
+def test_random_sample_location_and_vector_field_OT(FM):
+    # Test sample_location_and_conditional_flow functions
+    x0 = torch.randn(batch_size, 2, 2)
+    x1 = torch.randn(batch_size, 2, 2)
+    
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    t, xt, ut, eps = FM.sample_location_and_conditional_flow(x0, x1, return_noise=True)
+
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    x0, x1 = OTPlanSampler(method="exact").sample_plan(x0, x1)
+
     sigma_t = torch.FloatTensor([sigma]).reshape(-1, *([1] * (x0.dim() - 1))) 
     t_given = torch.rand(batch_size)
     t_given = t_given.reshape(-1, *([1] * (x0.dim() - 1)))
