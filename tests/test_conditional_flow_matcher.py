afe261833e17390830e7dd008bbfc4fe531481cf
Kilian <kilian.fatras.pro@gmail.com>
2023-11-14 11:56:09 -0500

    add sbcfm class test and test over t

--- tests/test_conditional_flow_matcher.py
+++ tests/test_conditional_flow_matcher.py
@@ -9,7 +9,8 @@ from torchcfm.optimal_transport import OTPlanSampler
 
 from torchcfm.conditional_flow_matching import (
     ConditionalFlowMatcher,
-    ExactOptimalTransportConditionalFlowMatcher
+    ExactOptimalTransportConditionalFlowMatcher,
+    SchrodingerBridgeConditionalFlowMatcher
 )
 
 seed = 1994
@@ -33,8 +34,8 @@ def test_I_CFM_class(FM):
 
     torch.manual_seed(seed)
     sigma_t = torch.FloatTensor([sigma]).reshape(-1, *([1] * (x0.dim() - 1))) 
-    t_given = torch.rand(batch_size)
-    t_given = t_given.reshape(-1, *([1] * (x0.dim() - 1)))
+    t_given_init = torch.rand(batch_size)
+    t_given = t_given_init.reshape(-1, *([1] * (x0.dim() - 1)))
 
     epsilon = torch.randn_like(x0)
     mu_t = t_given * x1 + (1 - t_given) * x0
@@ -44,6 +45,8 @@ def test_I_CFM_class(FM):
     assert torch.all(ut.eq(computed_ut))
     assert torch.all(xt.eq(computed_xt))
     assert torch.all(eps.eq(epsilon))
+    assert any(t_given_init == t)
+
 
 
 @pytest.mark.parametrize(
@@ -66,8 +69,8 @@ def test_OT_CFM_class(FM):
     x0, x1 = OTPlanSampler(method="exact").sample_plan(x0, x1)
 
     sigma_t = torch.FloatTensor([sigma]).reshape(-1, *([1] * (x0.dim() - 1))) 
-    t_given = torch.rand(batch_size)
-    t_given = t_given.reshape(-1, *([1] * (x0.dim() - 1)))
+    t_given_init = torch.rand(batch_size)
+    t_given = t_given_init.reshape(-1, *([1] * (x0.dim() - 1)))
 
     epsilon = torch.randn_like(x0)
     mu_t = t_given * x1 + (1 - t_given) * x0
@@ -77,5 +80,40 @@ def test_OT_CFM_class(FM):
     assert torch.all(ut.eq(computed_ut))
     assert torch.all(xt.eq(computed_xt))
     assert torch.all(eps.eq(epsilon))
-    
+    assert any(t_given_init == t)
 
+
+@pytest.mark.parametrize(
+    "FM",
+    [
+        SchrodingerBridgeConditionalFlowMatcher(sigma=sigma, 
+                                                ot_method="sinkhorn"),
+    ],
+)
+def test_SB_CFM_class(FM):
+    # Test sample_location_and_conditional_flow functions
+    x0 = torch.randn(batch_size, 2, 2)
+    x1 = torch.randn(batch_size, 2, 2)
+    
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    t, xt, ut, eps = FM.sample_location_and_conditional_flow(x0, x1, return_noise=True)
+
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    x0, x1 = OTPlanSampler(method="sinkhorn", reg=2*(sigma**2)).sample_plan(x0, x1)
+
+    sigma_t = torch.FloatTensor([sigma]).reshape(-1, *([1] * (x0.dim() - 1))) 
+    t_given_init = torch.rand(batch_size)
+    t_given = t_given_init.reshape(-1, *([1] * (x0.dim() - 1)))
+
+    epsilon = torch.randn_like(x0)
+    std = sigma_t * torch.sqrt(t_given * (1-t_given))
+    mu_t = t_given * x1 + (1 - t_given) * x0
+    computed_xt = mu_t + std * epsilon
+    computed_ut = (1 - 2*t_given)/(2*t_given*(1-t_given) + 1e-8) * (computed_xt - (t_given * x1 + (1 - t_given) * x0)) + x1 - x0
+
+    assert torch.all(ut.eq(computed_ut))
+    assert torch.all(xt.eq(computed_xt))
+    assert torch.all(eps.eq(epsilon))
+    assert any(t_given_init == t)
