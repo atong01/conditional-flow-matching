3d6ea276daf73a4539362eae6fd07344d85a4f99
Kilian <kilian.fatras.pro@gmail.com>
2023-11-14 14:52:44 -0500

    add FM class test

--- tests/test_conditional_flow_matcher.py
+++ tests/test_conditional_flow_matcher.py
@@ -10,7 +10,8 @@ from torchcfm.optimal_transport import OTPlanSampler
 from torchcfm.conditional_flow_matching import (
     ConditionalFlowMatcher,
     ExactOptimalTransportConditionalFlowMatcher,
-    SchrodingerBridgeConditionalFlowMatcher
+    SchrodingerBridgeConditionalFlowMatcher,
+    TargetConditionalFlowMatcher
 )
 
 seed = 1994
@@ -112,6 +113,37 @@ def test_SB_CFM_class(FM):
     mu_t = t_given * x1 + (1 - t_given) * x0
     computed_xt = mu_t + std * epsilon
     computed_ut = (1 - 2*t_given)/(2*t_given*(1-t_given) + 1e-8) * (computed_xt - (t_given * x1 + (1 - t_given) * x0)) + x1 - x0
+
+    assert torch.all(ut.eq(computed_ut))
+    assert torch.all(xt.eq(computed_xt))
+    assert torch.all(eps.eq(epsilon))
+    assert any(t_given_init == t)
+
+
+@pytest.mark.parametrize(
+    "FM",
+    [
+        TargetConditionalFlowMatcher(sigma=sigma),
+    ],
+)
+def test_FM_class(FM):
+    # Test sample_location_and_conditional_flow functions
+    x0 = torch.randn(batch_size, 2, 2)
+    x1 = torch.randn(batch_size, 2, 2)
+
+    torch.manual_seed(seed)
+    t, xt, ut, eps = FM.sample_location_and_conditional_flow(x0, x1, return_noise=True)
+
+    torch.manual_seed(seed)
+    sigma_t = torch.FloatTensor([sigma]).reshape(-1, *([1] * (x0.dim() - 1))) 
+    t_given_init = torch.rand(batch_size)
+    t_given = t_given_init.reshape(-1, *([1] * (x0.dim() - 1)))
+
+    epsilon = torch.randn_like(x0)
+    mu_t = t_given * x1
+    std = t_given * sigma_t - t_given + 1
+    computed_xt = mu_t + std * epsilon
+    computed_ut = (x1 - (1 - sigma_t) * computed_xt)/(1 - (1 - sigma_t) * t_given)
 
     assert torch.all(ut.eq(computed_ut))
     assert torch.all(xt.eq(computed_xt))
