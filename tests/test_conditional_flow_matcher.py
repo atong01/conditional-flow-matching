51a26ea9838c839309f96eed579fadc9d9662e72
guillaumehu <guillaumehuguet2@gmail.com>
2023-11-15 11:21:37 +0100

    merge test into one fn

--- tests/test_conditional_flow_matcher.py
+++ tests/test_conditional_flow_matcher.py
@@ -17,105 +17,35 @@ from torchcfm.conditional_flow_matching import (
 )
 from torchcfm.optimal_transport import OTPlanSampler
 
-seed = 1994
-batch_size = 128
-sigma = 0.5
+TEST_SEED = 1994
+TEST_BATCH_SIZE = 128
+TEST_SIGMA = 0.5
 
 
-@pytest.mark.parametrize(
-    "FM",
-    [
-        ConditionalFlowMatcher(sigma=sigma),
-    ],
-)
-def test_I_CFM_class(FM):
-    # Test sample_location_and_conditional_flow functions
-    list_data = [
-        [torch.randn(batch_size, 2), torch.randn(batch_size, 2)],
-        [torch.randn(batch_size, 2, 2, 2), torch.randn(batch_size, 2, 2, 2)],
-    ]
-    for x0, x1 in list_data:
-        torch.manual_seed(seed)
-        t, xt, ut, eps = FM.sample_location_and_conditional_flow(x0, x1, return_noise=True)
+def random_samples(n_dim, batch_size=TEST_BATCH_SIZE):
+    """Generate random samples of different dimensions."""
+    if n_dim == 1:
+        return [torch.randn(batch_size, 2), torch.randn(batch_size, 2)]
+    elif n_dim == 3:
+        return [torch.randn(batch_size, 2, 2, 2), torch.randn(batch_size, 2, 2, 2)]
 
-        torch.manual_seed(seed)
-        sigma_t = torch.FloatTensor([sigma]).reshape(-1, *([1] * (x0.dim() - 1)))
-        t_given_init = torch.rand(batch_size)
-        t_given = t_given_init.reshape(-1, *([1] * (x0.dim() - 1)))
 
-        epsilon = torch.randn_like(x0)
-        mu_t = t_given * x1 + (1 - t_given) * x0
+def computed_mu_x_u_t(method, x0, x1, t_given, sigma_t, epsilon):
+    if method == "vp_cfm":
+        mu_t = torch.cos(math.pi / 2 * t_given) * x0 + torch.sin(math.pi / 2 * t_given) * x1
         computed_xt = mu_t + sigma_t * epsilon
-        computed_ut = x1 - x0
+        computed_ut = (
+            math.pi
+            / 2
+            * (torch.cos(math.pi / 2 * t_given) * x1 - torch.sin(math.pi / 2 * t_given) * x0)
+        )
+    elif method == "t_cfm":
+        mu_t = t_given * x1
+        std = t_given * sigma_t - t_given + 1
+        computed_xt = mu_t + std * epsilon
+        computed_ut = (x1 - (1 - sigma_t) * computed_xt) / (1 - (1 - sigma_t) * t_given)
 
-        assert torch.all(ut.eq(computed_ut))
-        assert torch.all(xt.eq(computed_xt))
-        assert torch.all(eps.eq(epsilon))
-        assert any(t_given_init == t)
-
-
-@pytest.mark.parametrize(
-    "FM",
-    [
-        ExactOptimalTransportConditionalFlowMatcher(sigma=sigma),
-    ],
-)
-def test_OT_CFM_class(FM):
-    # Test sample_location_and_conditional_flow functions
-    list_data = [
-        [torch.randn(batch_size, 2), torch.randn(batch_size, 2)],
-        [torch.randn(batch_size, 2, 2, 2), torch.randn(batch_size, 2, 2, 2)],
-    ]
-    for x0, x1 in list_data:
-        torch.manual_seed(seed)
-        np.random.seed(seed)
-        t, xt, ut, eps = FM.sample_location_and_conditional_flow(x0, x1, return_noise=True)
-
-        torch.manual_seed(seed)
-        np.random.seed(seed)
-        x0, x1 = OTPlanSampler(method="exact").sample_plan(x0, x1)
-
-        sigma_t = torch.FloatTensor([sigma]).reshape(-1, *([1] * (x0.dim() - 1)))
-        t_given_init = torch.rand(batch_size)
-        t_given = t_given_init.reshape(-1, *([1] * (x0.dim() - 1)))
-
-        epsilon = torch.randn_like(x0)
-        mu_t = t_given * x1 + (1 - t_given) * x0
-        computed_xt = mu_t + sigma_t * epsilon
-        computed_ut = x1 - x0
-
-        assert torch.all(ut.eq(computed_ut))
-        assert torch.all(xt.eq(computed_xt))
-        assert torch.all(eps.eq(epsilon))
-        assert any(t_given_init == t)
-
-
-@pytest.mark.parametrize(
-    "FM",
-    [
-        SchrodingerBridgeConditionalFlowMatcher(sigma=sigma, ot_method="sinkhorn"),
-    ],
-)
-def test_SB_CFM_class(FM):
-    # Test sample_location_and_conditional_flow functions
-    list_data = [
-        [torch.randn(batch_size, 2), torch.randn(batch_size, 2)],
-        [torch.randn(batch_size, 2, 2, 2), torch.randn(batch_size, 2, 2, 2)],
-    ]
-    for x0, x1 in list_data:
-        torch.manual_seed(seed)
-        np.random.seed(seed)
-        t, xt, ut, eps = FM.sample_location_and_conditional_flow(x0, x1, return_noise=True)
-
-        torch.manual_seed(seed)
-        np.random.seed(seed)
-        x0, x1 = OTPlanSampler(method="sinkhorn", reg=2 * (sigma**2)).sample_plan(x0, x1)
-
-        sigma_t = torch.FloatTensor([sigma]).reshape(-1, *([1] * (x0.dim() - 1)))
-        t_given_init = torch.rand(batch_size)
-        t_given = t_given_init.reshape(-1, *([1] * (x0.dim() - 1)))
-
-        epsilon = torch.randn_like(x0)
+    elif method == "sb_cfm":
         std = sigma_t * torch.sqrt(t_given * (1 - t_given))
         mu_t = t_given * x1 + (1 - t_given) * x0
         computed_xt = mu_t + std * epsilon
@@ -126,77 +56,61 @@ def test_SB_CFM_class(FM):
             + x1
             - x0
         )
-
-        assert torch.all(ut.eq(computed_ut))
-        assert torch.all(xt.eq(computed_xt))
-        assert torch.all(eps.eq(epsilon))
-        assert any(t_given_init == t)
-
-
-@pytest.mark.parametrize(
-    "FM",
-    [
-        TargetConditionalFlowMatcher(sigma=sigma),
-    ],
-)
-def test_FM_class(FM):
-    # Test sample_location_and_conditional_flow functions
-    list_data = [
-        [torch.randn(batch_size, 2), torch.randn(batch_size, 2)],
-        [torch.randn(batch_size, 2, 2, 2), torch.randn(batch_size, 2, 2, 2)],
-    ]
-    for x0, x1 in list_data:
-        torch.manual_seed(seed)
-        t, xt, ut, eps = FM.sample_location_and_conditional_flow(x0, x1, return_noise=True)
-
-        torch.manual_seed(seed)
-        sigma_t = torch.FloatTensor([sigma]).reshape(-1, *([1] * (x0.dim() - 1)))
-        t_given_init = torch.rand(batch_size)
-        t_given = t_given_init.reshape(-1, *([1] * (x0.dim() - 1)))
-
-        epsilon = torch.randn_like(x0)
-        mu_t = t_given * x1
-        std = t_given * sigma_t - t_given + 1
-        computed_xt = mu_t + std * epsilon
-        computed_ut = (x1 - (1 - sigma_t) * computed_xt) / (1 - (1 - sigma_t) * t_given)
-
-        assert torch.all(ut.eq(computed_ut))
-        assert torch.all(xt.eq(computed_xt))
-        assert torch.all(eps.eq(epsilon))
-        assert any(t_given_init == t)
-
-
-@pytest.mark.parametrize(
-    "FM",
-    [
-        VariancePreservingConditionalFlowMatcher(sigma=sigma),
-    ],
-)
-def test_VPCFM_class(FM):
-    # Test sample_location_and_conditional_flow functions
-    list_data = [
-        [torch.randn(batch_size, 2), torch.randn(batch_size, 2)],
-        [torch.randn(batch_size, 2, 2, 2), torch.randn(batch_size, 2, 2, 2)],
-    ]
-    for x0, x1 in list_data:
-        torch.manual_seed(seed)
-        t, xt, ut, eps = FM.sample_location_and_conditional_flow(x0, x1, return_noise=True)
-
-        torch.manual_seed(seed)
-        sigma_t = torch.FloatTensor([sigma]).reshape(-1, *([1] * (x0.dim() - 1)))
-        t_given_init = torch.rand(batch_size)
-        t_given = t_given_init.reshape(-1, *([1] * (x0.dim() - 1)))
-
-        epsilon = torch.randn_like(x0)
-        mu_t = torch.cos(math.pi / 2 * t_given) * x0 + torch.sin(math.pi / 2 * t_given) * x1
+    elif method in ["exact_ot_cfm", "i_cfm"]:
+        mu_t = t_given * x1 + (1 - t_given) * x0
         computed_xt = mu_t + sigma_t * epsilon
-        computed_ut = (
-            math.pi
-            / 2
-            * (torch.cos(math.pi / 2 * t_given) * x1 - torch.sin(math.pi / 2 * t_given) * x0)
-        )
+        computed_ut = x1 - x0
 
-        assert torch.all(ut.eq(computed_ut))
-        assert torch.all(xt.eq(computed_xt))
-        assert torch.all(eps.eq(epsilon))
-        assert any(t_given_init == t)
+    return computed_xt, computed_ut
+
+
+def get_flow_matcher(method, sigma):
+    if method == "vp_cfm":
+        fm = VariancePreservingConditionalFlowMatcher(sigma=sigma)
+    elif method == "t_cfm":
+        fm = TargetConditionalFlowMatcher(sigma=sigma)
+    elif method == "sb_cfm":
+        fm = SchrodingerBridgeConditionalFlowMatcher(sigma=sigma, ot_method="sinkhorn")
+    elif method == "exact_ot_cfm":
+        fm = ExactOptimalTransportConditionalFlowMatcher(sigma=sigma)
+    elif method == "i_cfm":
+        fm = ConditionalFlowMatcher(sigma=sigma)
+    return fm
+
+
+def sample_plan(method, x0, x1, sigma):
+    if method == "sb_cfm":
+        x0, x1 = OTPlanSampler(method="sinkhorn", reg=2 * (sigma**2)).sample_plan(x0, x1)
+    elif method == "exact_ot_cfm":
+        x0, x1 = OTPlanSampler(method="exact").sample_plan(x0, x1)
+    return x0, x1
+
+
+@pytest.mark.parametrize("method", ["vp_cfm", "t_cfm", "sb_cfm", "exact_ot_cfm", "i_cfm"])
+@pytest.mark.parametrize("n_dim", [1, 3])
+def test_fm(method, n_dim):
+    sigma = TEST_SIGMA
+    batch_size = TEST_BATCH_SIZE
+    FM = get_flow_matcher(method, sigma)
+    x0, x1 = random_samples(batch_size=batch_size, n_dim=n_dim)
+    torch.manual_seed(TEST_SEED)
+    np.random.seed(TEST_SEED)
+    t, xt, ut, eps = FM.sample_location_and_conditional_flow(x0, x1, return_noise=True)
+
+    if method in ["sb_cfm", "exact_ot_cfm"]:
+        torch.manual_seed(TEST_SEED)
+        np.random.seed(TEST_SEED)
+        x0, x1 = sample_plan(method, x0, x1, sigma)
+
+    torch.manual_seed(TEST_SEED)
+    sigma_t = torch.FloatTensor([sigma]).reshape(-1, *([1] * (x0.dim() - 1)))
+    t_given_init = torch.rand(batch_size)
+    t_given = t_given_init.reshape(-1, *([1] * (x0.dim() - 1)))
+
+    epsilon = torch.randn_like(x0)
+    computed_xt, computed_ut = computed_mu_x_u_t(method, x0, x1, t_given, sigma_t, epsilon)
+
+    assert torch.all(ut.eq(computed_ut))
+    assert torch.all(xt.eq(computed_xt))
+    assert torch.all(eps.eq(epsilon))
+    assert any(t_given_init == t)
