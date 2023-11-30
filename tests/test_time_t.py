b319b480535c8ac40699984589a7ad53ca0dff85
Kilian <kilian.fatras.pro@gmail.com>
2023-11-30 10:27:28 -0500

    add return_noise test for guided functions

--- tests/test_time_t.py
+++ tests/test_time_t.py
@@ -49,22 +49,35 @@ def test_random_Tensor_t(FM):
         SchrodingerBridgeConditionalFlowMatcher(sigma=0.1),
     ],
 )
-def test_guided_random_Tensor_t(FM):
+@pytest.mark.parametrize("return_noise", [True, False])
+def test_guided_random_Tensor_t(FM, return_noise):
     # Test guided_sample_location_and_conditional_flow functions
     x0 = torch.randn(batch_size, 2)
     y0 = torch.randint(high=10, size=(batch_size, 1))
     x1 = torch.randn(batch_size, 2)
     y1 = torch.randint(high=10, size=(batch_size, 1))
 
-    torch.manual_seed(seed)
-    t_given = torch.rand(batch_size)
-    t_given, xt, ut, y0, y1 = FM.guided_sample_location_and_conditional_flow(
-        x0, x1, y0=y0, y1=y1, t=t_given
-    )
+    if return_noise:
+        torch.manual_seed(seed)
+        t_given = torch.rand(batch_size)
+        t_given, xt, ut, y0, y1, eps = FM.guided_sample_location_and_conditional_flow(
+            x0, x1, y0=y0, y1=y1, t=t_given, return_noise=return_noise
+        )
 
-    torch.manual_seed(seed)
-    t_random, xt, ut, y0, y1 = FM.guided_sample_location_and_conditional_flow(
-        x0, x1, y0=y0, y1=y1, t=None
-    )
+        torch.manual_seed(seed)
+        t_random, xt, ut, y0, y1, eps = FM.guided_sample_location_and_conditional_flow(
+            x0, x1, y0=y0, y1=y1, t=None, return_noise=return_noise
+        )
+    else:
+        torch.manual_seed(seed)
+        t_given = torch.rand(batch_size)
+        t_given, xt, ut, y0, y1 = FM.guided_sample_location_and_conditional_flow(
+            x0, x1, y0=y0, y1=y1, t=t_given
+        )
+
+        torch.manual_seed(seed)
+        t_random, xt, ut, y0, y1 = FM.guided_sample_location_and_conditional_flow(
+            x0, x1, y0=y0, y1=y1, t=None
+        )
 
     assert any(t_given == t_random)
