0b43559c8ba47aa0d07f006b340a8027686eabce
Alex Tong <alexandertongdev@gmail.com>
2024-07-10 18:54:22 -0400

    plumb through epsilon fixing + add test

--- torchcfm/conditional_flow_matching.py
+++ torchcfm/conditional_flow_matching.py
@@ -152,10 +152,7 @@ class ConditionalFlowMatcher:
         del t, xt
         return x1 - x0
 
-    def sample_noise_like(self, x):
-        return torch.randn_like(x)
-
-    def sample_location_and_conditional_flow(self, x0, x1, t=None, eps=None, return_noise=False):
+    def sample_location_and_conditional_flow(self, x0, x1, t=None, return_noise=False, eps=None):
         """
         Compute the sample xt (drawn from N(t * x1 + (1 - t) * x0, sigma))
         and the conditional vector field ut(x1|x0) = x1 - x0, see Eq.(15) [1].
@@ -169,8 +166,10 @@ class ConditionalFlowMatcher:
         (optionally) t : Tensor, shape (bs)
             represents the time levels
             if None, drawn from uniform [0,1]
-        return_noise : bool
+        (optionally) return_noise : bool
             return the noise sample epsilon
+        (optionally) eps: Tensor, shape (bs, *dim)
+            use a fixed noise vector epsilon
 
 
         Returns
@@ -190,7 +189,7 @@ class ConditionalFlowMatcher:
         assert len(t) == x0.shape[0], "t has to have batch size dimension"
 
         if eps is None:
-            eps = self.sample_noise_like(x0)
+            eps = torch.randn_like(x0)
         xt = self.sample_xt(x0, x1, t, eps)
         ut = self.compute_conditional_flow(x0, x1, t, xt)
         if return_noise:
@@ -235,7 +234,7 @@ class ExactOptimalTransportConditionalFlowMatcher(ConditionalFlowMatcher):
         super().__init__(sigma)
         self.ot_sampler = OTPlanSampler(method="exact")
 
-    def sample_location_and_conditional_flow(self, x0, x1, t=None, return_noise=False):
+    def sample_location_and_conditional_flow(self, x0, x1, t=None, return_noise=False, eps=None):
         r"""
         Compute the sample xt (drawn from N(t * x1 + (1 - t) * x0, sigma))
         and the conditional vector field ut(x1|x0) = x1 - x0, see Eq.(15) [1]
@@ -250,8 +249,10 @@ class ExactOptimalTransportConditionalFlowMatcher(ConditionalFlowMatcher):
         (optionally) t : Tensor, shape (bs)
             represents the time levels
             if None, drawn from uniform [0,1]
-        return_noise : bool
+        (optionally) return_noise : bool
             return the noise sample epsilon
+        (optionally) eps: Tensor, shape (bs, *dim)
+            use a fixed noise vector epsilon
 
         Returns
         -------
@@ -266,10 +267,10 @@ class ExactOptimalTransportConditionalFlowMatcher(ConditionalFlowMatcher):
         [1] Improving and Generalizing Flow-Based Generative Models with minibatch optimal transport, Preprint, Tong et al.
         """
         x0, x1 = self.ot_sampler.sample_plan(x0, x1)
-        return super().sample_location_and_conditional_flow(x0, x1, t, return_noise)
+        return super().sample_location_and_conditional_flow(x0, x1, t, return_noise, eps)
 
     def guided_sample_location_and_conditional_flow(
-        self, x0, x1, y0=None, y1=None, t=None, return_noise=False
+        self, x0, x1, y0=None, y1=None, t=None, return_noise=False, eps=None
     ):
         r"""
         Compute the sample xt (drawn from N(t * x1 + (1 - t) * x0, sigma))
@@ -289,8 +290,10 @@ class ExactOptimalTransportConditionalFlowMatcher(ConditionalFlowMatcher):
         (optionally) t : Tensor, shape (bs)
             represents the time levels
             if None, drawn from uniform [0,1]
-        return_noise : bool
+        (optionally) return_noise : bool
             return the noise sample epsilon
+        (optionally) eps: Tensor, shape (bs, *dim)
+            use a fixed noise vector epsilon
 
         Returns
         -------
@@ -306,10 +309,10 @@ class ExactOptimalTransportConditionalFlowMatcher(ConditionalFlowMatcher):
         """
         x0, x1, y0, y1 = self.ot_sampler.sample_plan_with_labels(x0, x1, y0, y1)
         if return_noise:
-            t, xt, ut, eps = super().sample_location_and_conditional_flow(x0, x1, t, return_noise)
+            t, xt, ut, eps = super().sample_location_and_conditional_flow(x0, x1, t, return_noise, eps)
             return t, xt, ut, y0, y1, eps
         else:
-            t, xt, ut = super().sample_location_and_conditional_flow(x0, x1, t, return_noise)
+            t, xt, ut = super().sample_location_and_conditional_flow(x0, x1, t, return_noise, eps)
             return t, xt, ut, y0, y1
 
 
@@ -469,7 +472,7 @@ class SchrodingerBridgeConditionalFlowMatcher(ConditionalFlowMatcher):
         ut = sigma_t_prime_over_sigma_t * (xt - mu_t) + x1 - x0
         return ut
 
-    def sample_location_and_conditional_flow(self, x0, x1, t=None, return_noise=False):
+    def sample_location_and_conditional_flow(self, x0, x1, t=None, return_noise=False, eps=None):
         """
         Compute the sample xt (drawn from N(t * x1 + (1 - t) * x0, sqrt(t * (1 - t))*sigma^2 ))
         and the conditional vector field ut(x1|x0) = (1 - 2 * t) / (2 * t * (1 - t)) * (xt - mu_t) + x1 - x0,
@@ -484,8 +487,10 @@ class SchrodingerBridgeConditionalFlowMatcher(ConditionalFlowMatcher):
         (optionally) t : Tensor, shape (bs)
             represents the time levels
             if None, drawn from uniform [0,1]
-        return_noise: bool
+        (optionally) return_noise: bool
             return the noise sample epsilon
+        (optionally) eps: Tensor, shape (bs, *dim)
+            use a fixed noise vector epsilon
 
 
         Returns
@@ -501,10 +506,10 @@ class SchrodingerBridgeConditionalFlowMatcher(ConditionalFlowMatcher):
         [1] Improving and Generalizing Flow-Based Generative Models with minibatch optimal transport, Preprint, Tong et al.
         """
         x0, x1 = self.ot_sampler.sample_plan(x0, x1)
-        return super().sample_location_and_conditional_flow(x0, x1, t, return_noise)
+        return super().sample_location_and_conditional_flow(x0, x1, t, return_noise, eps)
 
     def guided_sample_location_and_conditional_flow(
-        self, x0, x1, y0=None, y1=None, t=None, return_noise=False
+        self, x0, x1, y0=None, y1=None, t=None, return_noise=False, eps=None
     ):
         r"""
         Compute the sample xt (drawn from N(t * x1 + (1 - t) * x0, sigma))
@@ -524,8 +529,10 @@ class SchrodingerBridgeConditionalFlowMatcher(ConditionalFlowMatcher):
         (optionally) t : Tensor, shape (bs)
             represents the time levels
             if None, drawn from uniform [0,1]
-        return_noise : bool
+        (optionally) return_noise : bool
             return the noise sample epsilon
+        (optionally) eps: Tensor, shape (bs, *dim)
+            use a fixed noise vector epsilon
 
         Returns
         -------
@@ -541,10 +548,10 @@ class SchrodingerBridgeConditionalFlowMatcher(ConditionalFlowMatcher):
         """
         x0, x1, y0, y1 = self.ot_sampler.sample_plan_with_labels(x0, x1, y0, y1)
         if return_noise:
-            t, xt, ut, eps = super().sample_location_and_conditional_flow(x0, x1, t, return_noise)
+            t, xt, ut, eps = super().sample_location_and_conditional_flow(x0, x1, t, return_noise, eps)
             return t, xt, ut, y0, y1, eps
         else:
-            t, xt, ut = super().sample_location_and_conditional_flow(x0, x1, t, return_noise)
+            t, xt, ut = super().sample_location_and_conditional_flow(x0, x1, t, return_noise, eps)
             return t, xt, ut, y0, y1
 
 
