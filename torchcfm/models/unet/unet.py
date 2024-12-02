04e13d13ae7d5d2053aeb6d068267107e3f4b433
jhliu17 <junhaoliu17@gmail.com>
2024-12-02 10:37:38 -0800

    enable use_checkpoint flag for Attention Block

--- torchcfm/models/unet/unet.py
+++ torchcfm/models/unet/unet.py
@@ -270,7 +270,7 @@ class AttentionBlock(nn.Module):
         self.proj_out = zero_module(conv_nd(1, channels, channels, 1))
 
     def forward(self, x):
-        return checkpoint(self._forward, (x,), self.parameters(), True)
+        return checkpoint(self._forward, (x,), self.parameters(), self.use_checkpoint)
 
     def _forward(self, x):
         b, c, *spatial = x.shape
