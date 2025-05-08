b6a22f475fe5d2a77cab4b590e574e85d12df3c1
Peter Steinbach <p.steinbach@hzdr.de>
2025-05-08 18:02:57 +0200

    precommit hooks

--- tests/test_models.py
+++ tests/test_models.py
@@ -15,34 +15,31 @@ def test_initialize_unet():
 
     batch = torch.zeros((8, 1, 28, 28), dtype=torch.float32)
     label = torch.ones((8,), dtype=torch.long)
-    timesteps = torch.linspace(0,1,steps=8)
+    timesteps = torch.linspace(0, 1, steps=8)
 
-    _ = model(t= timesteps, x=batch, y=label)
+    _ = model(t=timesteps, x=batch, y=label)
 
 
 def test_initialize_mlp():
-
     model1 = MLP(dim=2, time_varying=True, w=64)
     batch = torch.ones((8, 3), dtype=torch.float32)
     output1 = model1(x=batch)
 
-    assert output1.shape == (8,2)
+    assert output1.shape == (8, 2)
 
     model2 = MLP(dim=2, w=64)
     batch = torch.ones((8, 2), dtype=torch.float32)
     output2 = model2(x=batch)
 
-    assert output2.shape == (8,2)
+    assert output2.shape == (8, 2)
 
 
 class mock_embedding(torch.nn.Module):
-
     def __init__(self, outdim=128):
         super().__init__()
         self.outdim = outdim
 
     def forward(self, inputs):
-
         batchsize = inputs.size(0)
         if len(inputs.shape) == 1:
             inputs = inputs.reshape((batchsize, 1))
@@ -51,22 +48,21 @@ class mock_embedding(torch.nn.Module):
 
 
 def test_conditional_model_without_integer_labels():
-
     model_channels = 32
     model = UNetModel(
         dim=(1, 28, 28),
         num_channels=model_channels,
         num_res_blocks=1,
         class_cond=False,
-        embedding_net = mock_embedding
+        embedding_net=mock_embedding,
     )
 
     x1 = torch.ones((8, 1, 28, 28), dtype=torch.float32)
     x0 = torch.randn_like(x1)
-    FM = ConditionalFlowMatcher(sigma=0.)
+    FM = ConditionalFlowMatcher(sigma=0.0)
     t, xt, ut = FM.sample_location_and_conditional_flow(x0, x1)
 
-    label = 42.1*torch.ones((8,)).float()
+    label = 42.1 * torch.ones((8,)).float()
 
     vt = model(t=t, x=xt, y=label)
 >>>>>>> 1dbfbd0 (more tests and floating point conditions)
--- torchcfm/models/unet/unet.py
+++ torchcfm/models/unet/unet.py
@@ -9,8 +9,15 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from .fp16_util import convert_module_to_f16, convert_module_to_f32
-from .nn import (avg_pool_nd, checkpoint, conv_nd, linear, normalization,
-                 timestep_embedding, zero_module)
+from .nn import (
+    avg_pool_nd,
+    checkpoint,
+    conv_nd,
+    linear,
+    normalization,
+    timestep_embedding,
+    zero_module,
+)
 
 
 class AttentionPool2d(nn.Module):
@@ -410,7 +417,7 @@ class UNetModel(nn.Module):
         use_scale_shift_norm=False,
         resblock_updown=False,
         use_new_attention_order=False,
-        embedding_net = nn.Identity
+        embedding_net=nn.Identity,
     ):
         super().__init__()
 
@@ -444,8 +451,9 @@ class UNetModel(nn.Module):
         if self.num_classes is not None:
             embedding_net = nn.Embedding if embedding_net == nn.Identity else embedding_net
             self.label_emb = embedding_net(num_classes, time_embed_dim)
-            assert not isinstance(self.label_emb, nn.Identity), f"for class-conditional networks, provide an embedding please!"
-
+            assert not isinstance(
+                self.label_emb, nn.Identity
+            ), f"for class-conditional networks, provide an embedding please!"
 
         ch = input_ch = int(channel_mult[0] * model_channels)
         self.input_blocks = nn.ModuleList(
@@ -604,9 +612,9 @@ class UNetModel(nn.Module):
         :return: an [N x C x ...] Tensor of outputs.
         """
         timesteps = t
-        #assert (y is not None) == (
+        # assert (y is not None) == (
         #    self.num_classes is not None
-        #), "must specify y if and only if the model is class-conditional"
+        # ), "must specify y if and only if the model is class-conditional"
         while timesteps.dim() > 1:
             print(timesteps.shape)
             timesteps = timesteps[:, 0]
@@ -622,8 +630,10 @@ class UNetModel(nn.Module):
         # (batchsize, 4*self.model_channels)
         emb = self.time_embed(embedded_timesteps)
 
-        if (y is not None):
-            assert y.shape[0] == x.shape[0], f"batch dimension of y ({y.shape[0]}) does not match x ({x.shape[0]})"
+        if y is not None:
+            assert (
+                y.shape[0] == x.shape[0]
+            ), f"batch dimension of y ({y.shape[0]}) does not match x ({x.shape[0]})"
             labels = self.label_emb(y)
             emb = emb + labels
 
@@ -861,6 +871,7 @@ class EncoderUNetModel(nn.Module):
 
 NUM_CLASSES = 1000
 
+
 # this overwrites UNetModel in __init__.py
 class UNetModelWrapper(UNetModel):
     def __init__(
@@ -882,7 +893,7 @@ class UNetModelWrapper(UNetModel):
         resblock_updown=False,
         use_fp16=False,
         use_new_attention_order=False,
-            embedding_net = nn.Identity
+        embedding_net=nn.Identity,
     ):
         """Dim (tuple): (C, H, W)"""
         image_size = dim[-1]
@@ -926,7 +937,7 @@ class UNetModelWrapper(UNetModel):
             use_scale_shift_norm=use_scale_shift_norm,
             resblock_updown=resblock_updown,
             use_new_attention_order=use_new_attention_order,
-            embedding_net = embedding_net
+            embedding_net=embedding_net,
         )
 
     def forward(self, t, x, y=None, *args, **kwargs):
