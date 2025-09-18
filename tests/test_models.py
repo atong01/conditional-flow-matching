d358cef6a1a4b0976370ec9c2d79a542108755d6
Peter Steinbach <p.steinbach@hzdr.de>
2025-09-18 17:10:51 +0200

    merge changes from main

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,3 +1,4 @@
+import torch
 from torchcfm.models import MLP
 from torchcfm.models.unet import UNetModel
 
@@ -10,4 +11,20 @@ def test_initialize_models():
         num_classes=10,
         class_cond=True,
     )
-    MLP(dim=2, time_varying=True, w=64)
+    model = MLP(dim=2, time_varying=True, w=64)
+
+
+def test_conditional_model_without_integer_labels():
+
+    model = UNetModel(
+        dim=(1, 28, 28),
+        num_channels=32,
+        num_res_blocks=1,
+        class_cond=False,
+    )
+    batch = torch.ones((8,28,28), dtype=torch.float32)
+    label = 42.1*torch.ones((8,)).float()
+    timesteps = torch.linspace(0,1,steps=5)
+
+    #forward run
+    x_ = model(t= timesteps, x=batch, y=label)
